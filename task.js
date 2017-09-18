6354a664072588088dba3cb13f5ff0198ea784e6
Anna <anna_prih@tut.by>
2017-09-18 19:50:23 +0300

    create summary.js

--- task.js
+++ task.js
@@ -1,4 +1,38 @@
 /**
  * Created by Anna on 13.09.2017.
  */
-const directory = process.argv[2];
+let directory = process.argv[2];
+let script = "";
+const fs = require('fs');
+const path = require('path');
+
+if(directory){
+    fs.stat(directory, function(error, pathDir) {
+        if(error || !(pathDir.isDirectory())){
+            console.log('Неверный путь')
+        }
+        else{
+            fs.readFile('summary.txt', function(error, data) {
+                if (error) {
+                    console.error('Ошибка');
+                } else {
+                    script =  data.toString();
+                    script = script.replace('flag', directory + '/');
+
+                    fs.writeFile(`${directory}\\summary.js`, script, function (error)
+                    {
+                        if(error){
+                            console.log('Ошибка при создании файла');
+                        }
+                    })
+                }
+            });
+
+        }
+    });
+}
+else {
+    console.log('Введите путь к директории');
+}
+
+
